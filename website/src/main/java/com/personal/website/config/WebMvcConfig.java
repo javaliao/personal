817014b3f90d1cc6d5b421de082383d27e55d2d4
lzw <lzw.liao.sh@baijiantest.com>
2020-03-18 13:47:05 +0800

    初始化搭建

--- website/src/main/java/com/personal/website/config/WebMvcConfig.java
+++ website/src/main/java/com/personal/website/config/WebMvcConfig.java
@@ -1,24 +1,21 @@
 package com.personal.website.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/**
- * @Author:Liaozhiwei
- * @Date: 2019/11/28 16:31
- */
+
 
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-    private static final Logger log = LoggerFactory.getLogger(WebMvcConfig.class);
-
+//    freemark配置
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         //如果打算将静态文件放在resources下面，则需要启用下面一段代码，如果将静态文件放在webapp下面，则什么也不用配置，直接可以访问
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
     }
+
 }
--- website/src/main/java/com/personal/website/controller/TestController.java
+++ website/src/main/java/com/personal/website/controller/TestController.java
@@ -1,20 +1,16 @@
 package com.personal.website.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * @Author:Liaozhiwei
- * @Date: 2020/3/17 19:07
- */
 @Controller
-@RequestMapping("/")
 @Api(value = "testController",description = "测试管理")
 public class TestController {
 
-    @RequestMapping("/test")
+    @ApiOperation(value = "测试接口")
+    @GetMapping("/test")
     public String test(){
         return "test";
     }
